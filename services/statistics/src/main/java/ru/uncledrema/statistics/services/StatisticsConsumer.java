8134cd2270db0f2ea066bb91baa4530955ab3d2f
UncleDrema <missl.wipiece@gmail.com>
2026-04-26 22:02:22 +0300

    feat: added topic creation and enabled events

--- services/statistics/src/main/java/ru/uncledrema/statistics/services/StatisticsConsumer.java
+++ services/statistics/src/main/java/ru/uncledrema/statistics/services/StatisticsConsumer.java
@@ -1,6 +1,8 @@
 package ru.uncledrema.statistics.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import tools.jackson.core.JacksonException;
@@ -9,6 +11,7 @@ import ru.uncledrema.statistics.dto.EventDto;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class StatisticsConsumer {
     private final ObjectMapper objectMapper;
     private final EventIngestionService eventIngestionService;
@@ -18,6 +21,7 @@ public class StatisticsConsumer {
             groupId = "${spring.kafka.consumer.group-id}"
     )
     public void consume(String payload) throws JacksonException {
+        log.info("Received event");
         EventDto event = objectMapper.readValue(payload, EventDto.class);
         eventIngestionService.ingest(event);
     }
